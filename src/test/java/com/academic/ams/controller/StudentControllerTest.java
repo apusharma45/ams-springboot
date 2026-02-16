9e79f0fd312336caf24f716c70a8b7b788dd0947
Apu Sharma <arapusharma45@gmail.com>
2026-02-16 17:44:43 +0600

    update: update all files

---
+++ src/test/java/com/academic/ams/controller/StudentControllerTest.java
@@ -0,0 +1,20 @@
+package com.academic.ams.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentControllerTest {
+
+    @Test
+    void updateStudentByTeacher() {
+    }
+
+    @Test
+    void updateMyProfile() {
+    }
+
+    @Test
+    void getMyProfile() {
+    }
+}
