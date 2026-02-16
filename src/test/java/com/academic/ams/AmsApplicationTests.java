93fd24d55a259fd515b1bb9a161b96471beb36c8
Apu Sharma <arapusharma45@gmail.com>
2026-02-16 19:41:07 +0600

    setup: setup testing environment

--- src/test/java/com/academic/ams/AmsApplicationTests.java
+++ src/test/java/com/academic/ams/AmsApplicationTests.java
@@ -2,12 +2,11 @@ package com.academic.ams;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AmsApplicationTests {
-
     @Test
-    void contextLoads() {
-    }
-
+    void contextLoads() {}
 }
